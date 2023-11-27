c7dc6433277cd8d66ce3001582251ecdbce06192
laufuentes <la.fuentes@gmail.com>
2023-11-27 11:35:31 +0100

    ICL corrected

--- fcts/J_fcts.py
+++ fcts/J_fcts.py
@@ -1,42 +1,66 @@
 import torch 
 import numpy as np
 import yaml 
-from fcts.utils import load_objects_from_yaml, reparametrized_expanded_params
+from fcts.utils import *
+from scipy.special import logit
 
-loaded_parameters = load_objects_from_yaml('../trained_parameters.yaml')
+loaded_parameters = load_objects_from_yaml('trained_parameters.yaml')
 
 n1 = loaded_parameters['n1']
 n2 = loaded_parameters['n2']
 nq = loaded_parameters['nq']
 nl = loaded_parameters['nl']
 
-indices_p = loaded_parameters['indices_p']
-indices_n = loaded_parameters['indices_n']
-indices_zeros = loaded_parameters['indices_zeros']
+indices_p = np.array(loaded_parameters['indices_p'])
+indices_n = np.array(loaded_parameters['indices_n'])
+indices_zeros = np.array(loaded_parameters['indices_zeros'])
 device = loaded_parameters['device']
 device2 = loaded_parameters['device2']
 
+inv_softplus=  lambda x: x + np.log(-np.expm1(-x))
+shrink_simplex_internal= (lambda p: 1 - p[:, :-1] / np.cumsum(p[:, ::-1], axis=1)[:, :0:-1])
+shrinkpow= lambda s: np.exp((np.arange(s.shape[1], 0, -1).reshape((1, -1))) * np.log(s))
+shrink_simplex= lambda p: shrinkpow(shrink_simplex_internal(p))
+
+
 def return_all_params(filepath): 
     loaded_parameters = load_objects_from_yaml(filepath)
-    nu_a = loaded_parameters['nu_a']
-    rho_a = loaded_parameters['rho_a']
-    nu_b = loaded_parameters['nu_b']
-    rho_b = loaded_parameters['rho_b']
-    nu_p = loaded_parameters['nu_p']
-    rho_p = loaded_parameters['rho_p']
-    nu_q = loaded_parameters['nu_q']
-    rho_q = loaded_parameters['rho_q']
-    tau_1 = loaded_parameters['tau_1']
-    tau_2 = loaded_parameters['tau_2']
-    mu_un = loaded_parameters['mu_un']
-    sigma_sq_a =loaded_parameters['sigma_sq_a']
-    sigma_sq_b =loaded_parameters['sigma_sq_b']
-    sigma_sq_p =loaded_parameters['sigma_sq_p']
-    sigma_sq_q =loaded_parameters['sigma_sq_q']
-    alpha_1 = loaded_parameters['alpha_1']
-    alpha_2 = loaded_parameters['alpha_2']
-    pi =loaded_parameters['pi']
-    return nu_a,rho_a,nu_b,rho_b,nu_p,rho_p,nu_q,rho_q,tau_1,tau_2,mu_un,sigma_sq_a,sigma_sq_b,sigma_sq_p,sigma_sq_q,alpha_1,alpha_2,pi
+    theta = np.concatenate(
+         (
+              ((loaded_parameters['mu_un'])[0][0],),
+              (inv_softplus(loaded_parameters['sigma_sq_a'][0][0]),),
+              (inv_softplus(loaded_parameters['sigma_sq_b'][0][0]),),
+              (inv_softplus(loaded_parameters['sigma_sq_p'][0][0]),),
+              (inv_softplus(loaded_parameters['sigma_sq_q'][0][0]),),
+              logit(shrink_simplex(np.array(loaded_parameters['alpha_1']).T).flatten()),
+              logit(shrink_simplex(np.array(loaded_parameters['alpha_2'])).flatten()),
+              logit(np.array(loaded_parameters['pi']).reshape(nq,nl).flatten()),
+         )
+    )
+
+    gamma = np.concatenate(
+        (
+            np.array(loaded_parameters['nu_a']).flatten(), 
+            inv_softplus(np.array(loaded_parameters['rho_a']).flatten()), 
+            np.array(loaded_parameters['nu_b']).flatten(),
+            inv_softplus(np.array(loaded_parameters['rho_b']).flatten()),
+            np.array(loaded_parameters['nu_p']).flatten(),
+            inv_softplus(np.array(loaded_parameters['rho_p']).flatten()),
+            np.array(loaded_parameters['nu_q']).flatten(),
+            inv_softplus(np.array(loaded_parameters['rho_q']).flatten()),
+            logit(shrink_simplex(np.array(loaded_parameters['tau_1'])).flatten()),
+            logit(shrink_simplex(np.array(loaded_parameters['tau_2'])).flatten()),
+        )
+    )
+
+    assert len(theta.shape) == 1
+    assert theta.shape[0] == 5 + nq - 1 + nl - 1 + nq * nl
+    assert len(gamma.shape) == 1
+    assert gamma.shape[0] == 4 * n1 + 4 * n2 + (n1 * (nq - 1)) + (
+        n2 * (nl - 1)
+    )
+
+    return np.concatenate((gamma, theta))
 
 def expectation_loglike_Y1(tau_1, alpha_1):
     return tau_1.sum(0) @ torch.log(alpha_1)
@@ -59,13 +83,16 @@ def expectation_loglike_Q(nu_q, rho_q, sigma_sq_q):
 
 
 def entropy_rx(rho_a, rho_b, rho_p, rho_q, tau_1, tau_2):
+    tau_i = tau_1.clone()
+    tau_i[torch.where(tau_i==0)]=1e-6
+
     return (1/2* (2 * n1 + 2 * n2)* 
         (torch.log(torch.tensor(2 * np.pi, dtype=torch.float32, device=device))+ 1)
         + 1 / 2 * torch.sum(torch.log(rho_a))
         + 1 / 2 * torch.sum(torch.log(rho_b))
         + 1 / 2 * torch.sum(torch.log(rho_p))
         + 1 / 2 * torch.sum(torch.log(rho_q))
-        - torch.sum(tau_1 * torch.log(tau_1))
+        - torch.sum(tau_i * torch.log(tau_i))
         - torch.sum(tau_2 * torch.log(tau_2)))
 
 
@@ -137,17 +164,9 @@ def expectation_loglike_X_cond_ABPY1Y2(
         
         return (expectation if expectation < 0 else torch.tensor(np.inf, device=device))
 
-def criteria(filepath):
-        ###MODIFIER ICI pour avoir juste les variables 
+def criteria(vector_of_parameters):
         (nu_a,rho_a,nu_b,rho_b,nu_p,rho_p,nu_q,rho_q,tau_1,tau_2,mu_un,sigma_sq_a,sigma_sq_b,sigma_sq_p,sigma_sq_q,alpha_1,alpha_2,
-            pi) = return_all_params(filepath)
-        if torch.any(tau_1.sum(dim=0) < 0.5):
-            print("One empty row class, algo stoped")
-            return torch.tensor(np.nan, device=device)
-        if torch.any(tau_2.sum(dim=0) < 0.5):
-            print("One empty column class, algo stoped")
-            return torch.tensor(np.nan, device=device)
-        
+            pi) = reparametrized_expanded_params(vector_of_parameters,n1,n2,nq,nl,device,)        
         
         expectation = (entropy_rx(rho_a, rho_b, rho_p, rho_q, tau_1, tau_2)
             + expectation_loglike_A(nu_a, rho_a, sigma_sq_a)[0]
@@ -158,5 +177,4 @@ def criteria(filepath):
             + expectation_loglike_Y2(tau_2, alpha_2)
             + expectation_loglike_X_cond_ABPY1Y2(mu_un,nu_a,nu_b,nu_p,nu_q,rho_a,rho_b,rho_p,rho_q,tau_1,tau_2,pi)
         )
-        
-        return -expectation
+        return expectation
